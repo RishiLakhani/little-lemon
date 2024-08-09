790d18444dad4553fba74a0e53af0205de47829b
Rishi Lakhani <rishilakhani2001@gmail.com>
2024-08-09 09:01:40 +0530

    Little Lemon Booking

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <React.Fragment>
+        <Nav />
+        <Main />
+        <Footer />
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default App;
